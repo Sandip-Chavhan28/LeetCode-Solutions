5871b413f698ddd48b4be6f5be107b77bb701c30
Sandip Chavhan <sandipchavhan7058@gmail.com>
2026-07-24 20:12:25 +0530

    Time: 0 ms (100%), Space: 43 MB (27.61%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,38 +1,47 @@
 class Solution {
-    public class info{
-        int idx ;
-        String s;
+    // public class info{
+    //     int idx ;
+    //     String s;
 
-        public info(int idx,String s){
-            this.idx = idx;
-            this.s = s;
-        }
-    }
+    //     public info(int idx,String s){
+    //         this.idx = idx;
+    //         this.s = s;
+    //     }
+    // }
     public String sortSentence(String s) {
-        int n= s.length();
-        StringBuilder sb = new StringBuilder();
-        PriorityQueue<info> pq = new PriorityQueue<>((a,b) -> a.idx - b.idx);
-        for(int i=0 ; i<n ; i++){
-            char ch = s.charAt(i);
-            if(Character.isWhitespace(ch)){
-                continue;
-            }
 
-            if(Character.isDigit(ch)){
-                pq.add(new info(ch-'0',sb.toString()));
-                sb = new StringBuilder();
-            }else{
-                sb.append(ch);
-            }
-        }
-        sb = new StringBuilder();
+        String a[] = s.split(" ");
+        String[] b = new String[ a.length];
 
-        while(!pq.isEmpty()){
-            sb.append(pq.remove().s);
-            sb.append(" ");
+        for(String x : a){
+            b[x.charAt(x.length()-1)-'1'] = x.substring(0,x.length()-1);
         }
 
-        sb.deleteCharAt(sb.length()-1);
-        return sb.toString();
+        return String.join(" ",b);
+        // int n= s.length();
+        // StringBuilder sb = new StringBuilder();
+        // PriorityQueue<info> pq = new PriorityQueue<>((a,b) -> a.idx - b.idx);
+        // for(int i=0 ; i<n ; i++){
+        //     char ch = s.charAt(i);
+        //     if(Character.isWhitespace(ch)){
+        //         continue;
+        //     }
+
+        //     if(Character.isDigit(ch)){
+        //         pq.add(new info(ch-'0',sb.toString()));
+        //         sb = new StringBuilder();
+        //     }else{
+        //         sb.append(ch);
+        //     }
+        // }
+        // sb = new StringBuilder();
+
+        // while(!pq.isEmpty()){
+        //     sb.append(pq.remove().s);
+        //     sb.append(" ");
+        // }
+
+        // sb.deleteCharAt(sb.length()-1);
+        // return sb.toString();
     }
 }
