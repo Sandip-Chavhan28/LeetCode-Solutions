7fba8b97dc9a4a03877ca71b7869f23630e5fa8e
Sandip Chavhan <sandipchavhan7058@gmail.com>
2026-08-01 20:12:29 +0530

    Time: 39 ms (20.57%), Space: 60.4 MB (7.71%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -7,6 +7,7 @@ class Solution {
             if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
+            if(nums[i] > 0) break;
             int j = i+1;
             int k = n-1;
             while(j<k){
