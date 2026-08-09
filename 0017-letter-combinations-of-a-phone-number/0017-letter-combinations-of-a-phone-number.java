b20ce6f3101f857c61e51cfa9f7dc03808d8cb73
Sandip Chavhan <sandipchavhan7058@gmail.com>
2026-08-09 20:46:14 +0530

    Time: 0 ms (100%), Space: 43.4 MB (77.61%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,61 +1,20 @@
 class Solution {
     List<String>list = new ArrayList<>();
-    public void generatecom(String digit,HashMap<Character,String>map){
-        if(digit.length() == 1){
-            String str = map.get(digit.charAt(0));
-            for(int i=0 ; i<str.length();i++){
-                String newstr = ""+str.charAt(i);
-                list.add(newstr);
-            }
-        }else if(digit.length() == 2){
-            String str = map.get(digit.charAt(0));
-            String str2 = map.get(digit.charAt(1));
-            for(int i=0 ; i<str.length();i++){
-                for(int j=0; j<str2.length();j++){
-                    String newstr = ""+str.charAt(i)+str2.charAt(j);
-                    list.add(newstr);
-                }
-            }
-        }else if(digit.length() == 3){
-            String str = map.get(digit.charAt(0));
-            String str2 = map.get(digit.charAt(1));
-            String str3 = map.get(digit.charAt(2));
-            for(int i=0 ; i<str.length();i++){
-                for(int j=0; j<str2.length();j++){
-                    for(int k=0 ; k<str3.length();k++){
-                        String newstr = ""+str.charAt(i)+str2.charAt(j)+str3.charAt(k);
-                        list.add(newstr);
-                    }
-                }
-            }
-        }else{
-            String str = map.get(digit.charAt(0));
-            String str2 = map.get(digit.charAt(1));
-            String str3 = map.get(digit.charAt(2));
-            String str4 = map.get(digit.charAt(3));
-            for(int i=0 ; i<str.length();i++){
-                for(int j=0; j<str2.length();j++){
-                    for(int k=0 ; k<str3.length();k++){
-                        for(int l=0 ; l<str4.length();l++){
-                            String newstr = ""+str.charAt(i)+str2.charAt(j)+str3.charAt(k)+str4.charAt(l);
-                            list.add(newstr);
-                        }
-                    }
-                }
-            } 
+    String lettes[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public void generatecom(String digits,int index,StringBuilder sd){
+        if(index == digits.length()){
+            list.add(sd.toString());
+            return;
+        }
+
+        String currstr = lettes[digits.charAt(index)-'0'];
+        for(int i=0 ; i<currstr.length() ;i++){
+            generatecom(digits,index+1,sd.append(currstr.charAt(i)));
+            sd.deleteCharAt(sd.length()-1);
         }
     }
     public List<String> letterCombinations(String digits) {
-        HashMap<Character,String>map = new HashMap<>();
-        map.put('2',"abc");
-        map.put('3',"def");
-        map.put('4',"ghi");
-        map.put('5',"jkl");
-        map.put('6',"mno");
-        map.put('7',"pqrs");
-        map.put('8',"tuv");
-        map.put('9',"wxyz");
-        generatecom(digits,map);
+        generatecom(digits,0,new StringBuilder());
         return list;
     }
 }
