73a5586834d46f8ab0e56aba799efc88d3348726
Sandip Chavhan <sandipchavhan7058@gmail.com>
2026-08-15 20:04:45 +0530

    Time: 20 ms (83.41%), Space: 56.8 MB (9.14%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -4,23 +4,24 @@ class Solution {
         int count = 0;
         int start = 0;
         int end = people.length-1;
-        boolean visit[] = new boolean[people.length];
+        // boolean visit[] = new boolean[people.length];
         while(start <= end){
             if(start<end && (people[start] + people[end]) <= limit){
                 count++;
-                visit[start] = true;
-                visit[end] = true;
+                // visit[start] = true;
+                // visit[end] = true;
                 start++;
                 end--;
             }else{
+                count++;
                 end--;
             }
         }
-        for(int i=start ;i<people.length;i++){
-            if(!visit[i] && people[i]<= limit){
-                count++;
-            }
-        }
+        // for(int i=start ;i<people.length;i++){
+        //     if(!visit[i] && people[i]<= limit){
+        //         count++;
+        //     }
+        // }
         return count;
     }
 }
