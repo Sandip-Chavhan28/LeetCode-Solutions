2c325acf9685542d96751f1b3fd41c2dbeaec81a
Sandip Chavhan <sandipchavhan7058@gmail.com>
2026-09-03 18:37:46 +0530

    Time: 2 ms (100%), Space: 121.9 MB (30.44%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,42 +1,21 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        Arrays.sort(nums1);
-        boolean iseven = true;
-        if(nums1[0] %2 != 0){
-            iseven =false;
+        int min = Integer.MAX_VALUE;
+        for(int i=0 ; i<nums1.length; i++){
+            if(nums1[i]<min){
+                min = nums1[i];
+            }
+        }
+        if(min % 2 == 1){
+            return true;
         }
 
-        if(iseven){
-            for(int i=0 ; i<nums1.length; i++){
-                boolean isfound = false;
-                if(nums1[i] %2 != 0){
-                    for(int j = i-1 ; j>=0 ; j--){
-                        if(nums1[i]-nums1[j]%2 == 0){
-                            isfound = true;
-                            break;
-                        }
-                    }
-                    if(!isfound){
-                        return false;
-                    }
-                }
-            }
-        }else{
-            for(int i=0 ; i<nums1.length; i++){
-                boolean isfound = false;
-                if(nums1[i] %2 == 0){
-                    for(int j = i-1 ; j>=0 ; j--){
-                        if(nums1[i]-nums1[j]%2 != 0){
-                            isfound = true;
-                            break;
-                        }
-                    }
-                    if(!isfound){
-                        return false;
-                    }
-                }
+        for(int i=0 ; i<nums1.length; i++){
+            if(nums1[i] %2 == 1){
+                return false;
             }
         }
+        
         return true;
     }
 }
