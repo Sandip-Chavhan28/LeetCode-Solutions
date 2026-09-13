56c817684149fc15e7c83c66b61fec4f9c1b7611
Sandip Chavhan <sandipchavhan7058@gmail.com>
2026-09-13 12:06:33 +0530

    Time: 1 ms (90.76%), Space: 42.2 MB (65.36%) - LeetHub

--- 0738-monotone-increasing-digits/0738-monotone-increasing-digits.java
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.java
@@ -1,44 +1,20 @@
 class Solution {
-    public void reverse(ArrayList<Integer>list){
-        int start = 0;
-        int end = list.size()-1;
-        while(start<end){
-            int temp = list.get(start);
-            list.set(start,list.get(end));
-            list.set(end,temp);
-            start++;
-            end--;
-        }
-    }
     public int monotoneIncreasingDigits(int n) {
-        ArrayList<Integer> list = new ArrayList<>();
-        while(n>0){
-            int r = n%10;
-            list.add(r);
-            n /= 10;
-        }
-        reverse(list);
+        char digit[] = String.valueOf(n).toCharArray();
 
-        int i = list.size()-1;
-        int mark = list.size();
+        int mark = digit.length;
 
-        while(i>0){
-            if(list.get(i-1) > list.get(i)){
-                list.set(i-1,list.get(i-1)-1);
+        for(int i=digit.length-1 ; i>0 ; i--){
+            if(digit[i-1] > digit[i]){
+                digit[i-1]--;
                 mark = i;
             }
-            i--;
         }
-       
 
-        for(int j = mark ; j<list.size() ;j++){
-            list.set(j,9);
+        for(int i=mark ;i<digit.length; i++){
+            digit[i] = '9';
         }
-        int number = 0;
 
-        for(int l=0 ; l<list.size() ;l++){
-            number = number * 10 + list.get(l);
-        }
-        return number;
+        return Integer.parseInt(new String(digit));
     }
 }
