99cd94c1d0ee8d3116ae027debcf97ce83c9ddb4
Sandip Chavhan <sandipchavhan7058@gmail.com>
2026-09-23 19:20:09 +0530

    Time: 2 ms (12.81%), Space: 43 MB (18.28%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean wordPattern(String pattern, String s) {
         HashMap<Character,String>map = new HashMap<>();
         String arr[] = s.split("\\s+");
-        HashSet<String> set = new HashSet<>();
 
         if(pattern.length() != arr.length){
             return false;
@@ -10,18 +9,15 @@ class Solution {
 
         for(int i=0 ; i<arr.length; i++){
             char ch = pattern.charAt(i);
-            if(!map.containsKey(ch)){
-                map.put(ch,arr[i]);
-            }else{
+            if(map.containsKey(ch)){
                 if(!map.get(ch).equals(arr[i])){
                     return false;
                 }
+            }else if(map.containsValue(arr[i])){
+                return false;
+            }else{
+                map.put(ch,arr[i]);
             }
-            set.add(map.get(ch));
-        }
-
-        if(set.size() != map.size()){
-            return false;
         }
         return true;
         
