1b31e50d973a6a8767d8a4580d6bb49f5256a221
Sandip Chavhan <sandipchavhan7058@gmail.com>
2026-09-23 20:10:10 +0530

    Time: 0 ms (100%), Space: 42 MB (72.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,26 +1,14 @@
 class Solution {
-    // public int climbStairs(int n) {
-    //     if(n == 1)  return 1;
-    //     int dp[] = new int[n+1];
-    //     dp[0] = 1 ; // this is a also one way not zero ways
-    //     dp[1] = 1 ;
-    //     for(int i=2 ; i<n+1 ; i++){
-    //         dp[i] = dp[i-1] + dp[i-2];
-    //     }
-    //     return dp[n];
-    // }
-
-    public int cal_climb(int n,int dp[]){
-        if(n == 0 || n == 1){
-            return 1;
+    public int climbStairs(int n) {
+        if(n <= 1)  return 1;
+        int dp[] = new int[n+1];
+        int prev1 = 1 ; // this is a also one way not zero ways
+        int prev2 = 1 ;
+        for(int i=2 ; i<n+1 ; i++){
+            int curr = prev1+prev2;
+            prev1 = prev2;
+            prev2 = curr;
         }
-        if(dp[n] != 0){
-            return dp[n];
-        }
-        dp[n] = cal_climb(n-1,dp) + cal_climb(n-2,dp);
-        return dp[n];
-    }
-    public int climbStairs(int n){
-       return  cal_climb(n,new int[n+1]);
+        return prev2;
     }
 }
